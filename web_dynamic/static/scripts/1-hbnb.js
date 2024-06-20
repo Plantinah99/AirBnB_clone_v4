d18fa2d64ff30b5f0c9f1c77caba19e4aeac83fa
Plantina99 <plantinahtshukudu@gmail.com>
2024-06-20 10:15:19 -0700

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,11 @@
-window.addEventListener('load', function () {
-  // task 2
-  const amenityIds = {};
-  $('input[type=checkbox]').change(function () {
-    if ($(this).prop('checked')) {
-      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else if (!$(this).prop('checked')) {
-      delete amenityIds[$(this).attr('data-id')];
-    }
-    if (Object.keys(amenityIds).length === 0) {
-      $('div.amenities h4').html('&nbsp');
+$('document').ready(function () {
+  let amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+      delete amenities[$(this).attr('data-id')];
     }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
   });
 });
